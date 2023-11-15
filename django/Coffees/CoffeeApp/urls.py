8e7e482cfcc17a6f1a60b3d41542edda3ed1b121
sairanjith101 <sairanjith101@gmail.com>
2023-11-15 17:43:37 +0530

    updated

--- django/Coffees/CoffeeApp/urls.py
+++ django/Coffees/CoffeeApp/urls.py
@@ -14,18 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-import CoffeeApp.urls
-
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import re_path
+from django.urls import path
+from CoffeeApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(CoffeeApp.urls)),
+    path('coffee/', views.home),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
